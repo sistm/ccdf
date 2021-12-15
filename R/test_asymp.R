73a2c4e919faf21d75705d3d63f9d6106ffc6ec6
kalidouBA <kalidou.ia.mlds@gmail.com>
2021-12-15 09:10:40 +0100

    one-step manual OLS for substantial speed-up

--- R/test_asymp.R
+++ R/test_asymp.R
@@ -38,107 +38,102 @@
 
 
 test_asymp <- function(Y, X, Z = NULL, sample_group=NULL,method=c("linear regression", "mixed model"),space_y = FALSE, number_y = length(unique(Y))){
-  
   Y <- as.numeric(Y)
-  
+  n_Y_all <- length(Y)
+
   if (space_y){
     y <- seq(ifelse(length(which(Y==0))==0,min(Y),min(Y[-which(Y==0)])),max(Y[-which.max(Y)]),length.out=number_y)
   }
   else{
     y <- sort(unique(Y))
   }
-  if(length(method)>1) method <- method[1]
-  stopifnot(method %in% c("linear regression","mixed model"))
+  n_y_unique <- length(y)
+
   # no covariates Z
-  if (is.null(Z)){ 
+  if (is.null(Z)){
     colnames(X) <- sapply(1:ncol(X), function(i){paste0('X',i)})
-    modelmat <- model.matrix(~.,data=X)
+    modelmat <- as.matrix(model.matrix(~.,data=X))
   }
-  
   # with covariates Z
   else{
     colnames(X) <- sapply(1:ncol(X), function(i){paste0('X',i)})
     colnames(Z) <- sapply(1:ncol(Z), function(i){paste0('Z',i)})
-    modelmat <- model.matrix(~.,data=cbind(X,Z))
+    modelmat <- as.matrix(model.matrix(~.,data=cbind(X,Z)))
   }
 
-  if(!is.null(sample_group))sample_group <- factor(sample_group[,1],levels = unique(sample_group[,1]))
-  
-  ind_X <- which(substring(colnames(modelmat),1,1)=="X")
-  #nb_fact <- colSums(modelmat[,ind_X])
-  
-  beta <- matrix(NA,(length(y)-1),length(ind_X))
-  indi_pi <- matrix(NA,length(Y),(length(y)-1))
-  
-  Phi <- (1/length(Y))*(t(as.matrix(modelmat))%*%as.matrix(modelmat))
-  H <- (solve(Phi)%*%t(as.matrix(modelmat))) # ginv
-  H <- H[ind_X,]
+  indexes_X <- which(substring(colnames(modelmat),1,1)=="X")
+  p_X <- length(indexes_X)
 
-  for (i in 1:(length(y)-1)){ # on fait varier le seuil
-    indi_Y <- 1*(Y<=y[i])
-    indi_pi[,i] <- indi_Y
-    #beta[i,] <- (1/length(indi_Y))*rowSums(sapply(1:length(Y),function(i){H[i]*indi_pi[i,]}))
-    if(method == "linear regression"){
-      reg <- lm(indi_Y ~ as.matrix(modelmat[,-1]))
-      beta[i,] <- reg$coefficients[ind_X]
-    }
-    else if(method == "mixed model"){
+  beta <- matrix(NA, (n_y_unique-1), p_X)
+  indi_pi <- matrix(0, n_Y_all, (n_y_unique-1))
+
+  if(method == "linear regression"){
+    index_one <- cumsum(sapply(sort(unique(Y)),
+                               function(elmt, vec) sum(vec == elmt),
+                               vec = sort(Y)))[-length(unique(Y))]
+
+    ix <- sort(Y, index.return = TRUE)$ix
+    indi_pi <- as.matrix(setNames(data.frame(lapply(1:(n_y_unique-1),
+                                                    function(i)
+                                                      replace(indi_pi[, i], ix[1:index_one[i]],
+                                                              rep(1, length(ix[1:index_one[i]]))))),
+                                  head(names(indi_pi), -1)))
+
+    modX_OLS <- modelmat[, c(1, indexes_X), drop = FALSE]
+    beta <- solve(crossprod(modX_OLS)) %*% t(modX_OLS) %*% indi_pi
+    test_stat <- sum(as.matrix(beta)[indexes_X,]^2) * n_Y_all
+  }else if(method == "mixed model"){
       if(is.null(sample_group)) {
         warning("sample_group is null",
                 "No random effects terms specified in formula")
       }else {
-        mod_mixed <- lmer(indi_Y ~ 1 + modelmat[, -1] + (1 | sample_group))
-        beta[i,] <- lme4::fixef(mod_mixed)[ind_X]
+        for (i in 1:(n_y_unique-1)){ # on fait varier le seuil
+          indi_Y <- 1*(Y<=y[i])
+          indi_pi[,i] <- indi_Y
+          mod_mixed <- lmer(indi_Y ~ 1 + modelmat[, -1] + (1 | sample_group))
+          beta[i,] <- lme4::fixef(mod_mixed)[ind_X]
+
+        }
+        beta <- as.vector(beta)
+        z <- (sqrt(n_Y_all))*beta
+        test_stat <- sum(t(z)*z)
       }
     }
-  }
-  
-  beta <- as.vector(beta)
+
+
   prop <- colMeans(indi_pi)
-  
-  if (is.null(dim(H))){
-    H_square <- sum(H^2)
-    Sigma <- sapply(1:(length(y)-1), function(i){sapply(1:((length(y)-1)*length(ind_X)), function(j){
-      if (i<=j){
-        (prop[i]-(prop[j]*prop[i]))
-      }
-      else{
-        (prop[j]-(prop[j]*prop[i]))
-      }
-    })})
-    Sigma <- (1/length(Y))*(H_square*Sigma)
+  Phi <- (1/n_Y_all)*(t(modelmat)%*%modelmat)
+  H <- (solve(Phi)%*%t(modelmat)) # ginv
+  H <- H[indexes_X, , drop=FALSE]
+
+  temp_Sigma <-  lapply(1:ncol(H),
+                        function(k){sapply(1:nrow(H),
+                                           function(s){sapply(1:nrow(H),
+                                                              function(r){H[s,k]*H[r,k]}
+                                           )}
+                        )}
+  )
+  sum_temp_Sigma <- Reduce(f = `+`, x = temp_Sigma)
+  if(is.null(dim(sum_temp_Sigma))){
+    sum_temp_Sigma <- matrix(sum_temp_Sigma)
   }
-  else{
-    
-    temp_Sigma <-  lapply(1:ncol(H), function(k){sapply(1:nrow(H), function(s){sapply(1:nrow(H), function(r){H[s,k]*H[r,k]})})})
-    sum_temp_Sigma <- temp_Sigma[[1]]
-    for (i in 2:ncol(H)){
-      sum_temp_Sigma <- sum_temp_Sigma + temp_Sigma[[i]]
+  ind_sig <- rep(1:(n_y_unique-1), p_X)
+
+  Sigma <- matrix(data = NA, nrow = (n_y_unique-1)*p_X,
+                  ncol = (n_y_unique-1)*p_X)
+  for (i in 1:((n_y_unique-1)*p_X)){
+    for (j in 1:i){
+      Sigma[i,j] <- Sigma[j,i] <- (1/n_Y_all)*sum_temp_Sigma[floor(i/(n_y_unique)+1),floor(j/(n_y_unique)+1)]*(prop[ind_sig[j]]-(prop[ind_sig[j]]*prop[ind_sig[i]]))
     }
-    
-    ind_sig <- rep(1:(length(y)-1),length(ind_X))
-    
-    Sigma <- matrix(NA,((length(y)-1)*length(ind_X)),((length(y)-1)*length(ind_X)))
-    for (i in 1:((length(y)-1)*length(ind_X))){
-      for (j in 1:((length(y)-1)*length(ind_X))){
-        if (i<=j){
-          Sigma[i,j] <- sum_temp_Sigma[floor(i/(length(y))+1),floor(j/(length(y))+1)]*(prop[ind_sig[i]]-(prop[ind_sig[j]]*prop[ind_sig[i]]))
-        }
-        else{
-          Sigma[i,j] <- sum_temp_Sigma[floor(i/(length(y))+1),floor(j/(length(y))+1)]*(prop[ind_sig[j]]-(prop[ind_sig[j]]*prop[ind_sig[i]]))
-        }
-      }
-    }
-    Sigma <- (1/length(Y))*Sigma
   }
+
   decomp <- eigen(Sigma)
-  A <- matrix(0,(length(ind_X)*(length(y)-1)),(length(ind_X)*(length(y)-1)))
-  diag(A) <- decomp$values
-  z <- (sqrt(length(Y)))*beta
-  STAT <- sum(t(z)*z)
-  pval <- survey::pchisqsum(STAT, lower.tail = FALSE, df = rep(1,length(diag(A))), a = diag(A), method = "saddlepoint")
-  
-  return(data.frame(raw_pval=pval,Stat=STAT))
+
+
+  pval <- survey::pchisqsum(test_stat, lower.tail = FALSE, df = rep(1,ncol(Sigma)),
+                            a = decomp$values, method = "saddlepoint")
+
+  return(data.frame("raw_pval" = pval, "Stat" = test_stat))
   
 }
 
